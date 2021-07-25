821a25ed0c2227a315653ce105002461fd9046d8
SprityON <71904569+SprityON@users.noreply.github.com>
2021-07-25 19:09:30 +0200

    query better

--- src/Utils.js
+++ src/Utils.js
@@ -49,12 +49,11 @@ module.exports = class Utils {
 
   static dbConnect() {
     return require('mysql').createPool({
-      connectionLimit: 500,
+      timeout: 10000,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: "mc_rpg",
-      debug: false
+      database: "mc_rpg"
     })
   }
 
