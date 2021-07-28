915bc7aa7d7b8dbc43f85b983feb6008510a3b22
SprityON <71904569+SprityON@users.noreply.github.com>
2021-07-28 12:13:13 +0200

    profile cmd begin

--- src/commands/game/rpg/profile.js
+++ src/commands/game/rpg/profile.js
@@ -9,17 +9,48 @@ module.exports = {
       callback(data)
     }, guild_id)
   },
-  aliases: [],
+  aliases: ['stats', 'p', 'pr'],
   permissions: ['SEND_MESSAGES'],
   timeout: 1000,
 
   execute(msg, args) {
     let embed = new BotClass.Discord.MessageEmbed()
+
+    Utils.query(`SELECT * FROM members WHERE member_id = ${msg.member.id}`, result => {
+      const toolsJSON = require('./tools/tools.json')
+      const rpg_name = result[0][0].rpg_name
+      const inventory = JSON.parse(result[0][0].inventory)
+      const progressBar = require('string-progressbar');
+
+      let durability_axe
+      let equipped_axe = () => {
+        let axe = inventory[1].tools.find(tool => Object.keys(tool)[0] === JSON.parse(result[0][0].lumbering_item).id)
+        let emoji = BotClass.client.emojis.cache.find(e => e.name === Object.keys(axe)[0])
+
+        if (!axe.code) return `${emoji}`
+        return `${emoji} (${progressBar.filledBar(axe.maxDurability, axe.currentDurability, 5)[0]})`
+      }
+      let durability_pickaxe
+      let equipped_pickaxe = () => {
+        if (result[0][0].mining_item) {
+          let pickaxe = inventory[1].tools.find(tool => Object.keys(tool)[0] === JSON.parse(result[0][0].mining_item).id)
+          let emoji = BotClass.client.emojis.cache.find(e => e.name === Object.keys(pickaxe)[0])
+
+          return `${emoji} (${progressBar.filledBar(pickaxe.maxDurability, pickaxe.currentDurability, 5)[0]})`
+        } else return 'None'
+      }
+      
+      embed.setAuthor(`${rpg_name}'s profile`, msg.author.avatarURL({dynamic: true}))
+      embed.addField(`Equipped Tools`, `Pickaxe: **${equipped_pickaxe()}**\nAxe: **${equipped_axe()}**\nSword: **None**`)
+
+
+      msg.inlineReply(embed)
+    })
   },
 
   help: {
-    enabled: false,
-    title: '',
-    description: ``,
+    enabled: true,
+    title: 'Profile',
+    description: `See your RPG profile!`,
   }
 }
